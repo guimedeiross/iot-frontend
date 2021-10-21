6e2a4cacd292a785af9dc43830840588180c3f71
Guilherme Schubert Medeiros <guilhermeschu10@gmail.com>
2021-10-21 11:28:42 -0300

    Iniciando projeto

---
+++ src/components/DashboardCard/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import './styles.css'
+
+export default () => {
+    return (
+        <section className="dashboard--container">
+            <div className="dashboard--card">Card</div>
+        </section>
+    )
+}
